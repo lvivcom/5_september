c7c4592055b9ce8746c6551c2494aa74f5e47966
Ischak <office@lvivcom.net>
2018-09-05 16:43:03 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -6,6 +6,5 @@ function you_num() {
         fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
     }
     document.getElementById('ansver').innerHTML = 'Ряд Фібоначчі = ' + fibonacci;
-    document.getElementById('ansver2').innerHTML = 'Ряд Фібоначчі = ' + fibonacci(n);
     
     }
